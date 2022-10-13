341a13702a473b54c80e8316458b6d4a16fba19a
VadimTan <v.tanasiev1@gmail.com>
2022-10-13 16:25:06 +0300

    Modified gulpfile by VadimTan

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
-const gulp = require('gulp'); 
-const cssnano = require('gulp-cssnano'); 
-const sass = require('gulp-sass')(require('sass')); 
-const concat = require('gulp-concat'); 
+const gulp = require('gulp');
+const cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const minify = require('gulp-minify');
 const { series } = require('gulp')
 const { watch } = require('gulp');
 
-gulp.task('sass', function(){    
-    return gulp.src('app/*.scss')       
+gulp.task('sass', function () {
+    return gulp.src('app/*.scss')
         .pipe(sass())
-        .pipe(concat('global.css'))       
-        .pipe(cssnano())       
-        .pipe(gulp.dest('dist/css')); 
+        .pipe(concat('global.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', function(){
+gulp.task('js', function () {
     return gulp.src('src/main.js')
         .pipe(sass())
         .pipe(cssnano())
         .pipe(gulp.dest('dist/js'))
-}); 
-
-gulp.task('html', function(){
-return gulp.src('src/index.html')
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(gulp.dest('dist/html'))
 });
 
-gulp.task('images', function() {
-    gulp.src(['src/assets/images/*.jpg'])
-      .pipe(sass())
-      .pipe(cssnano())
-      .pipe(gulp.dest('dist/assets/images'))
-  });
+gulp.task('html', function () {
+    return gulp.src('src/index.html')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/html'))
+});
 
-  gulp.task('watch', function() {
+gulp.task('images', function () {
+    gulp.src(['src/assets/images/*.jpg'])
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/assets/images'))
+});
+
+gulp.task('watch', function () {
     gulp.watch('src/*.js', gulp.series('js'));
     gulp.watch('src/assets/scss/*.scss', gulp.series('sass'));
 });
